bdc5433d45b6785e86cd87402bcea41122dbb1e7
Azamat Margiev <129996890+AzMargiev@users.noreply.github.com>
2023-04-27 18:58:10 -0400

    Add files via upload

---
+++ src/class3/E5Modules.java
@@ -0,0 +1,17 @@
+package class3;
+
+public class E5Modules {
+    public class E5Modules {
+        public static void main(String[] args) {
+
+            int number1 = 11;
+            int number2 = 5;
+
+            System.out.println(number1/number2);
+            System.out.println(number1%number2);
+
+
+
+
+        }
+}
---
+++ src/class3/E7StringConcat.java
@@ -0,0 +1,13 @@
+package class3;
+
+public class E7StringConcat {
+    public class E7StringConcat {
+        public static void main(String[] args) {
+
+            String firstName = "Alex ";
+            String lastName = "Rash";
+            String myFullName = firstName+lastName;
+            System.out.println(myFullName);
+
+        }
+}
